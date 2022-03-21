8b4fb3853054340f44e3f6c80c24445463e1ff79
Ericlis Gonçalves <feodpv02000@odontoprev.com.br>
2022-03-21 12:46:56 -0300

    Atualização!

---
+++ teste.py
@@ -0,0 +1,12 @@
+a = 5
+b = 10
+trocaDeValores = 0
+
+print("Valor de A: ", a)
+print("Valor de B: ", b)
+
+trocaDeValores = a
+a = b
+b = trocaDeValores
+print("Valor trocado de A: ", a)
+print("Valor trocado de B: ", b)
